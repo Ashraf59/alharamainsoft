9217bbf3ca72517cf6a804b7d25d7d6d43dca19f
Ashraf59 <ashrafams59b@gmail.com>
2023-11-17 00:37:22 +0600

    sdffdf

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -3,26 +3,25 @@ import About from "./About/About";
 import Banner from "./Banner/Banner";
 import DevelopBusiness from "./DevelopBusiness/DevelopBusiness";
 
-import OurTech from "./OurTech/OurTech";
+
 import Services from "./Services/Services";
-import WhyChooseUs from "./whyChooseUs/whyChooseUs";
+
 import OurTeam from "./OurTeam/OurTeam";
 import Portfolio from "./Portfolio/Portfolio";
 import ServiceMarqueeText from "./ServiceMarqueeText/ServiceMarqueeText";
 import TechnologyMarquee from "./TechnologyMarquee/TechnologyMarquee";
+import WhyChooseUs from "./whyChooseUs/whyChooseUs";
 
 const Home = () => {
     return (
         <div>
           <Banner></Banner>
           <ServiceMarqueeText></ServiceMarqueeText>
-          {/* <OurTech></OurTech> */}
           <TechnologyMarquee></TechnologyMarquee>
           <Services></Services>
-          <WhyChooseUs></WhyChooseUs>
+        <WhyChooseUs></WhyChooseUs>
           <About></About>
           <DevelopBusiness></DevelopBusiness>
-         
          <OurTeam></OurTeam>
          <Portfolio></Portfolio>
         </div>
